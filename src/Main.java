f3ef935eeaddb8915589811ffbe0b1e963cae8ed
SexyDoGyun <kimdk0903@naver.com>
2025-04-16 13:35:42 +0900

    refactor: 숫자, 연산자 입력 while 문 안으로 넣기(반복)

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
-
-        System.out.print("사칙연산 기호를 입력하세요(예: +,-,*,/) : ");
-        char operator = sc.nextLine().charAt(0);
-
-        double result = 0.0;
         while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("사칙연산 기호를 입력하세요(예: +,-,*,/) : ");
+            char operator = sc.nextLine().charAt(0);
+
+
+            double result = 0.0;
             if (operator == '+') {
                 result = num1 + num2;
             } else if (operator == '-') {
@@ -26,8 +28,10 @@ public class Main {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                     break;
                 } else {
-                    result = num1 / num2;
+                    result = (double) num1 / num2;
                 }
+            } else {
+                System.out.println("올바른 연산자를 입력해주세요.");
             }
         }
         }
