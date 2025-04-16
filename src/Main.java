58283bfc96a5fcd41d8d89b391445932883d3b29
SexyDoGyun <kimdk0903@naver.com>
2025-04-16 12:58:56 +0900

    feat: 사칙연산 : 곱셈 구현

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,8 @@ public class Main {
                 result = num1 + num2;
             } else if (operator == '-') {
                 result = num1 - num2;
+            } else if (operator == '*') {
+                result = num1 * num2;
             }
         }
         }
