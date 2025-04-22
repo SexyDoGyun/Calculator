3a5c94d8db42a867ef3248cc337093cbc77ba747
SexyDoGyun <kimdk0903@naver.com>
2025-04-22 11:55:15 +0900

    refactor: OperatorType enum 상수에 람다식 추가

--- src/lv3/OperatorType.java
+++ src/lv3/OperatorType.java
@@ -1,15 +1,25 @@
 package lv3;
 
+import java.util.function.DoubleBinaryOperator;
+
 public enum OperatorType {
-    ADD('+'),
-    MINUS('-'),
-    MULTIPLY('*'),
-    DIVIDE('/');
+    ADD('+', (a, b) -> a + b),
+    MINUS('-', (a, b) -> a - b),
+    MULTIPLY('*', (a, b) -> a * b),
+    DIVIDE('/', (a, b) -> {
+        if (b == 0) {
+            throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+        } else {
+            return a / b;
+        }
+    });
 
     private final char operator;
+    private final DoubleBinaryOperator operation;
 
-    OperatorType(char operator) {
+    OperatorType(char operator, DoubleBinaryOperator operation) {
         this.operator = operator;
+        this.operation = operation;
     }
 
 
@@ -31,4 +41,9 @@ public enum OperatorType {
         }
     }
 
+    public double calculate(double a, double b) {
+        return operation.applyAsDouble(a, b);
+    }
+
 }
+
